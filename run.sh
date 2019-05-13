887f3eb80e3c23fe92e24e6a3421543de0dffe6e
hantiaotiao <1779754921@qq.com>
2019-05-13 22:30:35 +0800

    run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+port=8001
+
+git pull
+pip install -r requirements.txt
+kill -9 $(lsof -ti tcp:$port)
+nohup uwsgi --http :$port --chdir ./ --home=./venv  --module MovieKgAPI.wsgi  &
+tail -f nohup.out
+
+
