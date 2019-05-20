357fcc4f5a9c93aff03c9a004f956a711d07a721
xorsum <1779754921@qq.com>
2019-05-20 16:33:31 +0800

    update run.sh: install virtual environment automatically

--- run.sh
+++ run.sh
@@ -3,6 +3,14 @@
 port=8001
 
 git pull
+if [ ! -d "./venv" ]; then
+	python3 -m venv venv
+fi
+source ./venv/bin/activate
 pip install -r requirements.txt
 kill -9 $(lsof -ti tcp:$port)
 nohup uwsgi --http :$port --chdir ./ --home=./venv  --module MovieKgAPI.wsgi  &
+
+echo -e  "\033[33mthe project started, you can run \"tail -fn 20  nohup.out\" to follow the log\033[0m"
+
+
