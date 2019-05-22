0e2cab03d80cc86f68f9b9d5b522e2543e6f837d
xorsum <1779754921@qq.com>
2019-05-22 13:49:09 +0800

    add: docker-compose.yml

--- MovieKgAPI/settings/prod.py
+++ MovieKgAPI/settings/prod.py
@@ -7,10 +7,10 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'movie_kg',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'POST': 3306,
+        'NAME': os.environ.setdefault('MYSQL_DATABASE', 'movie_kg'),
+        'USER':  os.environ.setdefault('MYSQL_USERNAME', 'root'),
+        'PASSWORD':  os.environ.setdefault('MYSQL_PASSWORD', '123456'),
+        'HOST':  os.environ.setdefault('MYSQL_HOST', '127.0.0.1'),
+        'PORT':  os.environ.setdefault('MYSQL_PORT', '3306'),
     }
 }
