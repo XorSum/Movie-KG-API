34b65a025c371ae12ca6b8b14e38b6855ad1e673
hantiaotiao <1779754921@qq.com>
2019-05-31 21:53:50 +0800

    refactor: subjects

--- Subject/tests.py
+++ Subject/tests.py
@@ -1,3 +1,21 @@
+from datetime import date
 from django.test import TestCase
+from Subject.models import Movie, Person, MoviePerson
+from Subject.views import search_movie
 
-# Create your tests here.
+
+class SubjectTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        movie = Movie.objects.get_or_create(year=2019, title="神奇五合一", rating=10.0,
+                                            summary="年度大戏", original_title="神奇五合一")[0]
+        person = Person.objects.get_or_create(name="han", gender="男", name_en="han",
+                                              summary="一个人", birthday=date.today(), born_place="fc")[0]
+        MoviePerson.objects.create(movie=movie, person=person, role='主演')
+
+    def test_json(self):
+        movie = Movie.objects.get(title="神奇五合一")
+        print(movie.json(show_all=True))
+        person = Person.objects.get(name="han")
+        print(person.json(show_all=True))
--- Subject/views.py
+++ Subject/views.py
@@ -7,40 +7,27 @@ from Subject.models import Movie, Person
 from utils.json_response import json_response
 
 
-def hello(*args,**kwargs):
+def hello(*args, **kwargs):
     return HttpResponse('congratulations! it works!')
 
 
 def get_subject(request, subjectId):
     try:
         subject = Person.objects.get(id=subjectId)
-        return json_response(subject.json(show_movie=True), 200)
+        return json_response(subject.json(show_all=True), 200)
     except ObjectDoesNotExist:
         try:
             subject = Movie.objects.get(id=subjectId)
-            return json_response(subject.json(show_person=True), 200)
+            return json_response(subject.json(show_all=True), 200)
         except ObjectDoesNotExist:
             return json_response('Subject not found exception', 400)
-
-
-def search_subject(request):
-    try:
-        name = request.GET['name']
-    except:
-        return json_response('need param \'name\'', 500)
-    result = []
-    for movie in Movie.objects.filter(title=name).all():
-        result.append(movie.json(show_person=True))
-    for person in Person.objects.filter(name=name).all():
-        result.append(person.json(show_movie=True))
-    return json_response(result, 200)
 
 
 def get_movie(request, movieId):
     logging.info('get movie, id=' + str(movieId))
     try:
         movie = Movie.objects.get(id=movieId)
-        return json_response(movie.json(show_person=True, show_video=True), 200)
+        return json_response(movie.json(show_all=True), 200)
     except Exception as e:
         repr(e)
         return json_response('error', 500)
@@ -50,9 +37,22 @@ def get_person(request, personId):
     logging.info('get person, id=' + str(personId))
     try:
         person = Person.objects.get(id=personId)
-        return json_response(person.json(show_movie=True), 200)
+        return json_response(person.json(show_all=True), 200)
     except Exception as e:
         return json_response('error', 500)
+
+
+def search_subject(request):
+    try:
+        name = request.GET['name']
+    except:
+        return json_response('need param \'name\'', 500)
+    result = []
+    for movie in Movie.objects.filter(title__icontains=name).all():
+        result.append(movie.json(show_all=True))
+    for person in Person.objects.filter(name__icontains=name).all():
+        result.append(person.json(show_all=True))
+    return json_response(result, 200)
 
 
 def search_movie(request):
@@ -60,8 +60,8 @@ def search_movie(request):
     try:
         title = parse.unquote(request.GET['title'])
         logging.info('search movie, title=' + str(title))
-        for movie in Movie.objects.filter(title=title).all():
-            result.append(movie.json(show_person=True))
+        for movie in Movie.objects.filter(title__icontains=title).all():
+            result.append(movie.json(show_all=True))
         return json_response(result, 200)
     except:
         return json_response([], 500)
@@ -72,8 +72,8 @@ def search_person(request):
     try:
         name = parse.unquote(request.GET['name'])
         logging.info('search person, name=' + str(name))
-        for person in Person.objects.filter(name=name).all():
-            result.append(person.json(show_movie=True))
+        for person in Person.objects.filter(title__icontains=name).all():
+            result.append(person.json(show_all=True))
         return json_response(result, 200)
     except:
         return json_response([], 500)
