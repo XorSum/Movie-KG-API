dc17b4c78c0ff46118b3d3d838fc68b4e5aa82b4
xorsum <1779754921@qq.com>
2019-06-01 15:00:59 +0800

    modify: docker

--- docker-start.sh
+++ docker-start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-python3 manage.py makemigrations
 python3 manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'admin')" | python3 manage.py shell
-
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'Administrator', 'admin')" | python3 manage.py shell
 uwsgi --http :8000 --chdir ./ --module MovieKgAPI.wsgi
